b8414a61c3e7f2beb58f6cdf2c16db2aa74f4e8e
Aditya9103 <adityakumarsingh84260@gmail.com>
2026-07-17 17:00:59 +0530

    refactor: remove start time parameter from hero video iframe source URL

--- client/src/components/home/HeroVideo.jsx
+++ client/src/components/home/HeroVideo.jsx
@@ -7,7 +7,7 @@ const HeroVideo = () => {
         {/* Video Background */}
         <iframe
           className="absolute top-1/2 left-1/2 w-full h-full md:w-[100vw] md:h-[56.25vw] md:min-h-[120vh] md:min-w-[215vh] -translate-x-1/2 -translate-y-1/2 pointer-events-none opacity-100 contrast-[1.05] saturate-[1.1] md:scale-105"
-          src="https://www.youtube.com/embed/xJ05rWqlS8w?autoplay=1&mute=1&loop=1&playlist=xJ05rWqlS8w&start=18&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&vq=hd2160&hd=1"
+          src="https://www.youtube.com/embed/xJ05rWqlS8w?autoplay=1&mute=1&loop=1&playlist=xJ05rWqlS8w&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&vq=hd2160&hd=1"
           title="Background Animation"
           allow="autoplay; encrypted-media; picture-in-picture"
           frameBorder="0"
